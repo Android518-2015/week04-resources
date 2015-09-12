6d08f4585101d1063d603c2d1ae5273d7dd5f696
Tricia <pcampbell.dawson@gmail.com>
2015-09-12 15:10:18 -0400

    initial commit

---
+++ src/cs518/samples/imageswap/MainActivity.java
@@ -0,0 +1,93 @@
+package cs518.samples.imageswap;
+
+/**
+ * App uses various resources
+ * drawables
+ * colour
+ * string       
+ * raw     (sound)
+ *
+ * in xml:  
+ *  use an alternate layout  landscape / portrait
+ *  use dimensions in layout 
+ *  @author tricia
+ */
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	ImageButton bt01, bt02;
+	TextView tv;
+	MediaPlayer mp;
+	int bt01image, bt01colour;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		bt01 = (ImageButton) findViewById(R.id.imageButton1);
+		bt02 = (ImageButton) findViewById(R.id.imageButton2);
+		
+		tv = (TextView) findViewById(R.id.message);
+		
+		if ( (mp = MediaPlayer.create(this, R.raw.meow)) == null) {
+			  ImageButton bt03 = (ImageButton) findViewById(R.id.imageButton3);
+			  // if problem with media player then disable tard button
+			  bt03.setEnabled(false);
+	     }
+		bt01image = R.drawable.cleannallboo;
+		bt01colour = R.color.indigo;
+		swapimage();
+	}
+
+    protected void onPause() {
+		super.onPause();
+    	// releasing the MediaPlayer resources
+    	if (mp != null) { 
+    		mp.release();
+    	}
+    }
+    
+	public void button1Clicked(View view) {
+		Toast.makeText(this, "Button1 clicked", Toast.LENGTH_SHORT).show();
+		swapimage();	
+	}
+	public void button2Clicked(View view) {
+		Toast.makeText(this, "Button2 clicked", Toast.LENGTH_SHORT).show();
+		swapimage();
+	}
+	public void button3Clicked(View view) {
+		Toast.makeText(this, "Button3 clicked", Toast.LENGTH_LONG).show();
+		mp.start();
+		
+	}
+	public void swapimage() {
+		// swap the two images in place and changes the message text colour.
+		if (bt01image == R.drawable.cleannallboo) {
+			bt01.setImageResource(R.drawable.cleanallyay);
+			bt02.setImageResource(R.drawable.cleannallboo);
+			bt01image = R.drawable.cleanallyay;
+			tv.setTextColor(getResources().getColor(R.color.lightcoral));
+			 
+		} else {
+			bt02.setImageResource(R.drawable.cleanallyay);
+			bt01.setImageResource(R.drawable.cleannallboo);
+			bt01image = R.drawable.cleannallboo;
+			tv.setTextColor(getResources().getColor(R.color.indigo));
+		}
+	}
+	public void rotateScreen(View view) {
+		if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+	       setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		else 
+			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	}  //rotateScreen()
+	
+}
